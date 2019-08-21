66eb061400e8fcb9ec9251b955144cabab8a602d
Sherin-Denzil <nysherin.denzil@gmail.com>
2019-08-21 18:07:51 -0400

    created repository array with pokemon objects inside.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-alert('Hello world');
-var favoriteFood="pizza";
-document.write(favoriteFood);
+var repository = [
+  { name: "bulbasaur",height: 0.7,types: ["grass", "poison"]},
+  { name: "venusaur",height: 1.2,types: ["grass","poison"]},
+  { name: "charizard", height: 1.5, types: ["fire","flying"]},
+  { name: "charmeleon", height: 1.6, types: ["fire"]},
+  { name:"caterpie",height: 1.8, types: ["bug"]},
+  { name:"blastoise",height: 2.5,types: ["water"]},
+  { name:"pikachu",height:1.7,types:["electric"]}   
+];
