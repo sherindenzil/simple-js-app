97f73ac626976a6474c2cb06bc66b5abd176a9ba
Sherin-Denzil <nysherin.denzil@gmail.com>
2019-08-23 13:11:09 -0400

    added 'for' loops and 'conditions' to javascript file for displaying repository array.

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,31 @@ var repository = [
   { name: "charmeleon", height: 1.6, types: ["fire"]},
   { name:"caterpie",height: 1.8, types: ["bug"]},
   { name:"blastoise",height: 2.5,types: ["water"]},
-  { name:"pikachu",height:1.7,types:["electric"]}   
+  { name:"pikachu",height:1.7,types:["electric"]}
 ];
+for(i=0;i<repository.length;i++){
+  var size;
+  if (repository[i].height>2) {
+    size = "Wow, that’s big!";
+  }else {
+    size = "Small Pokemon";
+  }
+  var result;
+  for (var typeItem = 0; typeItem < repository[i].types.length; typeItem++) {
+      if (repository[i].types[typeItem] == "grass") {
+        result = '<span style="color:green;"> ';
+      } else if (repository[i].types[typeItem] == "fire") {
+        result = '<span style="color:red;"> ';
+      } else if (repository[i].types[typeItem] == "water") {
+        result = '<span style="color:blue;"> ';
+      } else if (repository[i].types[typeItem] == "poison") {
+        result = '<span style="color:green;"> ';
+      }else if (repository[i].types[typeItem] == "electric") {
+        result = '<span style="color:rgb(255, 204, 0);"> ';
+      }else if (repository[i].types[typeItem] == "bug") {
+        result = '<span style="color:rgb(0, 255, 0);"> ';
+      }
+
+    }
+  document.write('<p>' + repository[i].name + ' ' +'( height:' + repository[i].height +')  -' +' '+ size + result +' '+repository[i].types +'<br>' + '</p>');
+}
