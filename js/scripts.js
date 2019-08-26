951de754d861451534b1ced2678c5aadf6d70e37
Sherin-Denzil <nysherin.denzil@gmail.com>
2019-08-26 18:13:38 -0400

    Updated scripts.js with 'forEach' functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
-var repository = [
+  var repository = [
   { name: "bulbasaur",height: 0.7,types: ["grass", "poison"]},
   { name: "venusaur",height: 1.2,types: ["grass","poison"]},
   { name: "charizard", height: 1.5, types: ["fire","flying"]},
   { name: "charmeleon", height: 1.6, types: ["fire"]},
   { name:"caterpie",height: 1.8, types: ["bug"]},
-  { name:"blastoise",height: 2.5,types: ["water"]},
-  { name:"pikachu",height:1.7,types:["electric"]}
-];
-for(i=0;i<repository.length;i++){
+  { name:"blastoise",height: 2.5,types: ["water"]}
+       ];
+
+repository.forEach(function(item){
   var size;
-  if (repository[i].height>2) {
+  if (item.height>2) {
     size = "Wow, that’s big!";
   }else {
-    size = "Small Pokemon";
+      size = "Small Pokemon";
   }
   var result;
-  for (var typeItem = 0; typeItem < repository[i].types.length; typeItem++) {
-      if (repository[i].types[typeItem] == "grass") {
+  item.types.forEach (function(typeItem) {
+      if (typeItem == "grass") {
         result = '<span style="color:green;"> ';
-      } else if (repository[i].types[typeItem] == "fire") {
+      } else if (typeItem == "fire") {
         result = '<span style="color:red;"> ';
-      } else if (repository[i].types[typeItem] == "water") {
+      } else if (typeItem == "water") {
         result = '<span style="color:blue;"> ';
-      } else if (repository[i].types[typeItem] == "poison") {
+    } else if (typeItem == "poison") {
         result = '<span style="color:green;"> ';
-      }else if (repository[i].types[typeItem] == "electric") {
+      }else if (typeItem == "electric") {
         result = '<span style="color:rgb(255, 204, 0);"> ';
-      }else if (repository[i].types[typeItem] == "bug") {
+      }else if (typeItem == "bug") {
         result = '<span style="color:rgb(0, 255, 0);"> ';
-      }
+   }
 
-    }
-  document.write('<p>' + repository[i].name + ' ' +'( height:' + repository[i].height +')  -' +' '+ size + result +' '+repository[i].types +'<br>' + '</p>');
-}
+    });
+  document.write('<p>' + item.name + ' ' +'( height:' + item.height +')  -' +' '+ size + result +' '+item.types +'<br>' + '</p>');
+});
+console.log(repository);
