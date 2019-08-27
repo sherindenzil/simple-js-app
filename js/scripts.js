8130b45668c889902643de3dbb97023c72965282
Sherin-Denzil <nysherin.denzil@gmail.com>
2019-08-27 11:02:51 -0400

    Updated file with IIFE functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var pokemonRepository = (function () {
+
   var repository = [
   { name: "bulbasaur",height: 0.7,types: ["grass", "poison"]},
   { name: "venusaur",height: 1.2,types: ["grass","poison"]},
@@ -6,8 +8,32 @@
   { name:"caterpie",height: 1.8, types: ["bug"]},
   { name:"blastoise",height: 2.5,types: ["water"]}
        ];
+    function add (pokemon) {
+      if(typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    )
+    {
+     repository.push(pokemon);
+   }
+ }
+function getAll (){
+  return repository;
+}
+ return{
+   add: add,
+   getAll: getAll
+  };
+})();
 
-repository.forEach(function(item){
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+console.log(pokemonRepository.getAll());
+
+
+
+pokemonRepository.getAll().forEach(function(item){
   var size;
   if (item.height>2) {
     size = "Wow, that’s big!";
@@ -33,4 +59,3 @@ repository.forEach(function(item){
     });
   document.write('<p>' + item.name + ' ' +'( height:' + item.height +')  -' +' '+ size + result +' '+item.types +'<br>' + '</p>');
 });
-console.log(repository);
